a63b0dae98676245f2e209d55270920466e72d1e
Ashraf-Jr <76624556+Ashraf-Jr@users.noreply.github.com>
2022-03-28 21:29:54 +0200

    Add files via upload

---
+++ Model.php
@@ -0,0 +1,14 @@
+<?php
+abstract class Model{
+    protected $dbh;
+
+    public function __construct(){
+        $this->dbh = new Database;
+    }
+
+
+    public function getDBH(){
+        return $this->dbh;
+    }
+    
+}
